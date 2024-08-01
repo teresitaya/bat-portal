9af3d01e73e3d7393f0f4b5463f96bae8773aa33
Maria Rosales <mrosales@itopia.com>
2024-08-01 14:38:08 -0400

    Initial test component

--- src/app/bat-test/bat-test.component.ts
+++ src/app/bat-test/bat-test.component.ts
@@ -1,12 +1,85 @@
-import { Component } from '@angular/core';
-
+import { JsonPipe } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { StorageTestComponent } from '../storage-test/storage-test.component';
 @Component({
   selector: 'app-bat-test',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule, JsonPipe, StorageTestComponent],
   templateUrl: './bat-test.component.html',
-  styleUrl: './bat-test.component.scss'
+  styleUrl: './bat-test.component.scss',
 })
 export class BatTestComponent {
+  testIsRunning: boolean = false;
+  testIsFinished: boolean = false;
+  browserInformation: any;
+  networkInformation: any;
+  serviceWorkerInformation: any;
+ 
 
+  async runTest() {
+    this.testIsRunning = true;
+    this.testIsFinished = false;
+    this.getBrowserInformation();
+    //await this.getNetworkInfo();
+    this.checkServiceWorker();
+    setTimeout(() => {
+      this.testIsRunning = false;
+      this.testIsFinished = true;
+    }, 500);
+  }
+
+  getBrowserInformation() {
+    this.browserInformation = navigator.userAgent;
+  }
+
+  async getNetworkInfo() {
+    let ipAddress = 'N/A';
+    try {
+      const response = await fetch('https://api.ipify.org?format=json');
+      const data = await response.json();
+      ipAddress = data.ip;
+    } catch (error) {
+      console.error('Error fetching IP address:', error);
+    }
+
+    const connection =
+      (navigator as any).connection ||
+      (navigator as any).mozConnection ||
+      (navigator as any).webkitConnection;
+    const connectionType = connection ? connection.effectiveType : 'N/A';
+    const downlink = connection ? connection.downlink : 'N/A';
+    const rtt = connection ? connection.rtt : 'N/A';
+    const downlinkMax = connection ? connection.downlinkMax : 'N/A';
+
+    this.networkInformation = {
+      ipAddress,
+      connectionType,
+      downlink,
+      rtt,
+      downlinkMax,
+    };
+  }
+
+  private isServiceWorkerSupported() {
+    return 'serviceWorker' in navigator;
+}
+
+private areServiceWorkersRegistered() {
+    if ('serviceWorker' in navigator) {
+        return navigator.serviceWorker.getRegistrations().then((registrations) => {
+            return registrations.length > 0;
+        });
+    }
+    return Promise.resolve(false);
+}
+
+  async checkServiceWorker() {
+    this.serviceWorkerInformation = {
+      serviceWorkerSupported: this.isServiceWorkerSupported(),
+      serviceWorkersRegistered: await this.areServiceWorkersRegistered() + ' (is returning always false)',
+    };
+  }
+
+ 
 }
---
+++ src/app/storage-test/storage-test.component.ts
@@ -0,0 +1,85 @@
+import { JsonPipe } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-storage-test',
+  standalone: true,
+  imports: [JsonPipe],
+  templateUrl: './storage-test.component.html',
+  styleUrl: './storage-test.component.scss'
+})
+export class StorageTestComponent implements OnInit {
+
+  storageInformation: any;
+
+  ngOnInit(): void {
+    this.getStorageInformation();
+  }
+
+
+  areCookiesEnabled() {
+    document.cookie = 'testcookie=1';
+    const cookiesEnabled = document.cookie.indexOf('testcookie=') !== -1;
+    if (cookiesEnabled) {
+      document.cookie =
+        'testcookie=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+    }
+    return cookiesEnabled;
+  }
+
+  getStorageInformation() {
+    const localStorageAvailable = this.isLocalStorageAvailable();
+    const sessionStorageAvailable = this.isSessionStorageAvailable();
+    const indexedDBAvailable = this.isIndexedDBAvailable();
+    const cookies = this.areCookiesEnabled();
+    let localStorageSpace = 0;
+    let sessionStorageSpace = 0;
+    if (localStorageAvailable) {
+      localStorageSpace = this.getStorageSpace(localStorage);
+    }
+    if (sessionStorageAvailable) {
+      sessionStorageSpace = this.getStorageSpace(sessionStorage);
+    }
+    this.storageInformation = {
+      localStorageAvailable,
+      sessionStorageAvailable,
+      localStorageSpace,
+      sessionStorageSpace,
+      cookies,
+      indexedDBAvailable
+    };
+  }
+
+  isLocalStorageAvailable() {
+    try {
+      localStorage.setItem('test', 'test');
+      localStorage.removeItem('test');
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+  isSessionStorageAvailable() {
+    try {
+      sessionStorage.setItem('test', 'test');
+      sessionStorage.removeItem('test');
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  getStorageSpace(storage: any) {
+    let total = 0;
+    for (let x in storage) {
+      if (storage.hasOwnProperty(x)) {
+        total += (storage[x].length + x.length) * 2;
+      }
+    }
+    return total;
+  }
+
+  isIndexedDBAvailable() {
+    return 'indexedDB' in window;
+  }
+}
