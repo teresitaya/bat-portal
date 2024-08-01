e748282bdc0fe88736d6c8eb6307a912e4944b0f
Maria Rosales <mrosales@itopia.com>
2024-08-01 10:35:23 -0400

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { BatTestComponent } from './bat-test/bat-test.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: BatTestComponent
+    }
+];
---
+++ src/app/bat-test/bat-test.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bat-test',
+  standalone: true,
+  imports: [],
+  templateUrl: './bat-test.component.html',
+  styleUrl: './bat-test.component.scss'
+})
+export class BatTestComponent {
+
+}
